1abc57a58015df9c84392934dcabeccedbcb3339
ZhengGang85129 <a0910555246@gmail.com>
2024-11-06 06:58:14 -0800

    modify

--- submitter.sh
+++ submitter.sh
@@ -18,6 +18,6 @@ nvidia-smi
 which bash
 source /global/homes/z/zhenggan/miniconda3/etc/profile.d/conda.sh
 conda activate exatrkx-gpu
-traintrack ./configs/pipeline-PU200_best-smeared.yaml #> output-PU200_GNN-0099.log 2>&1 
-mv stage_performance.png stage_performance-PU200-smeared.png
-mv stage_performance.pdf stage_performance-PU200-smeared.pdf
+traintrack configs/pipeline-mixing_PU200_smeared.yaml #> output-PU200_GNN-0099.log 2>&1 
+mv stage_performance.png stage_performance-PU200-smeared_all.png
+mv stage_performance.pdf stage_performance-PU200-smeared_all.pdf
